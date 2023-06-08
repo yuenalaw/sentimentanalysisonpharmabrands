dceaf28e5a87a577b256fb97e6e9eb4927c9483e
Yue Law <X258338@one.merckgroup.com>
2023-06-08 15:46:08 +0900

    also keep track of topics

--- consumer.py
+++ consumer.py
@@ -9,45 +9,6 @@ from datetime import datetime, timedelta
 three_years_ago = datetime.now() - timedelta(days=365 * 3)
 starting_timestamp = int(three_years_ago.timestamp() * 1000)
 
-# Schema for the incoming data
-schema = StructType([StructField("message", StringType())])
-
-# Read the data from kafka
-df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "Opdivo, Keytruda, Tagrisso, Alecensa, Tecentriq, Bevacizumab, Stelara, Zytiga, Lynparza, Lixiana") \
-    .option("startingOffsets", starting_timestamp) \
-    .option("header", "true") \
-    .load() \
-    .selectExpr("CAST(value AS STRING) as message")
-
-df = df.withColumn("value", from_json("message", schema)) #new value column with json of message
-
-# Pre-processing the data
-pre_process = udf(
-    lambda x: re.sub(r'[^A-Za-z\n ]|(http\S+)|(www.\S+)', '', x.lower().strip()).split(), ArrayType(StringType())
-)
-
-df = df.withColumn("cleaned_data", pre_process(df.message)).dropna()
-
-# Load the pre-trained model
-pipeline_model = PipelineModel.load(path_to_model)
-# Make predictions
-prediction = pipeline_model.transform(df)
-# Select the columns of interest
-prediction = prediction.select(prediction.message, prediction.prediction)
-
-# Print prediction in console
-prediction \
-    .writeStream \
-    .format("console") \
-    .outputMode("update") \
-    .start() \
-    .awaitTermination()
-
-
 if __name__ == "__main__":
     findspark.init()
 
@@ -61,3 +22,41 @@ if __name__ == "__main__":
         .appName("TwiterPharmaBrandsSentimentAnalysis") \
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
         .getOrCreate()
+    
+    # Schema for the incoming data
+    schema = StructType([StructField("message", StringType())])
+
+    # Read the data from kafka
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "Opdivo, Keytruda, Tagrisso, Alecensa, Tecentriq, Bevacizumab, Stelara, Zytiga, Lynparza, Lixiana") \
+        .option("startingOffsets", starting_timestamp) \
+        .option("header", "true") \
+        .load() \
+        .selectExpr("CAST(value AS STRING) as message", "CAST(topic AS STRING) as topic") #keep track of message AND topic
+
+    df = df.withColumn("value", from_json("message", schema)) #new value column with json format of message
+
+    # Pre-processing the data, udf = user defined function
+    pre_process = udf(
+        lambda x: re.sub(r'[^A-Za-z\n ]|(http\S+)|(www.\S+)', '', x.lower().strip()).split(), ArrayType(StringType())
+    )
+
+    df = df.withColumn("cleaned_data", pre_process(df.message)).dropna()
+
+    # Load the pre-trained model
+    pipeline_model = PipelineModel.load(path_to_model)
+    # Make predictions, resulting df will contain additional columns like "prediction"
+    prediction = pipeline_model.transform(df)
+    # Select the columns of interest - original message, and also the prediction
+    prediction = prediction.select(prediction.topic, prediction.message, prediction.prediction)
+
+    # Print prediction in console
+    prediction \
+        .writeStream \
+        .format("console") \
+        .outputMode("update") \
+        .start() \
+        .awaitTermination()
